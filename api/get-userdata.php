cf726e7fdffbfd996fa30b61896f6858e193a1c6
TeslaQ1 <104022375+TeslaZXC@users.noreply.github.com>
2024-01-21 22:50:40 +0300

    Add files via upload

---
+++ api/get-userdata.php
@@ -0,0 +1,13 @@
+<?php
+$filepath = "api/data/_users.json";
+$userid = $_GET['id'];
+
+$data_array = file_exists($filepath) ? json_decode(file_get_contents($filepath), true) : [];
+
+foreach ($data_array as $user) {
+    if ($user['id'] == $userid) {
+        echo json_encode($user);
+        break;
+    }
+}
+?>
---
+++ api/save-userdata.php
@@ -0,0 +1,28 @@
+<?php
+$secretKey = 'L36wAPaLDc';
+$requestSecretKey = $_SERVER['HTTP_X_SECRET_KEY'];
+
+if ($requestSecretKey !== $secretKey) {
+    http_response_code(401);
+    echo 'Unauthorized';
+    exit();
+}
+
+$filepath = "api/data/_users.json";
+$data = json_decode(file_get_contents('php://input'), true);
+
+$users = file_exists($filepath) ? json_decode(file_get_contents($filepath), true) : [];
+
+$foundUser = array_filter($users, function ($user) use ($data) {
+    return $user['id'] == $data['id'];
+});
+
+if (count($foundUser) > 0) {
+    $foundUserKey = array_keys($foundUser)[0];
+    $users[$foundUserKey] = $data;
+} else {
+    $users[] = $data;
+}
+
+file_put_contents($filepath, json_encode($users, JSON_PRETTY_PRINT));
+?>
